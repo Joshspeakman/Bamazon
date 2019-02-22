7042983edc231219da59bbfdb736ddd5b0374d2b
Joshua Speakman <44591690+Joshspeakman@users.noreply.github.com>
2019-02-21 20:12:47 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # Bamazon
+Bamazon
+
+# Overview
+Bamazon is an Amazon-like storefront which utilizes MySQL and node.js. The app will take in orders from customers and deplete stock from the store's inventory.
+
+# Bamazon Customer View
+Running ' node bamazonCustomer.js` will first display all of the items available for sale. This will include the ids, names, departments, prices of products for sale and the stock quantity.
+
+- The app will then prompt users with two messages.
+- The first will ask for the ID of the product that they would like to buy.
+- The second message will ask how many units of the product they would like to buy.
+
+
+![gify3](https://user-images.githubusercontent.com/43361200/51454456-fb4fe200-1d01-11e9-9245-25d18a74db05.gif)
+
+
+
+- Once the customer has placed the order, the application will check if the store has sufficient product items to meet the customer's request.
+
+- If there is enough stock_quantitiy to fulfill the order the app will show the user the name, department, price, quantity, and toatal amount for the purchase.
+
+
+
+
+
+
+
+![gify4](https://user-images.githubusercontent.com/43361200/51454455-fb4fe200-1d01-11e9-9935-b61dcb2d13cf.gif)
+
+
+
+
+
+
+- If not, the app will log the phrase Insufficient quantity!, and prevent the order from going through.
+
+![gifyin](https://user-images.githubusercontent.com/43361200/51454583-b11b3080-1d02-11e9-99d2-d1f168488b1d.gif)
+
+
+
+
+
+
+
+
+- The app will then promt the user "Are you sure you would like to purchase this item and quantity?"
+- If the user selects yes the app will log the phrase "Transaction completed. Thank you."
+- The database will be updated with the subtraction of the quanitity of the product chosen by the user.
+
+![gifyyes](https://user-images.githubusercontent.com/43361200/51454684-37d00d80-1d03-11e9-8cc9-2efc79f766d8.gif)
+
+
+
+
+
+
+
+
+
+-If the user choses no, the app will log the phrase "No worries. Maybe next time!"
+
+![gifynexttime](https://user-images.githubusercontent.com/43361200/51454582-b11b3080-1d02-11e9-9b08-8e8b22726a5b.gif)
+
+
+
+
+
+
+
+
+# Technology Used
+
+- Node.js
+- NPM packages
+  - inquirer 
+  - mysql 
+  - cliTable
---
+++ bamazonCustomer.js
@@ -0,0 +1,161 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var cliTable = require("cli-table");
+
+
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'dude',
+    database: 'bamazon'
+
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('Connected as id: ' + connection.threadId);
+
+
+
+
+
+    showProducts();
+
+
+
+
+
+
+
+});
+
+// shows the database in a cli table
+function showProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        var table = new cliTable({
+            head: ["Item Number", "Product Name", "Department", "Price", "Stock"],
+            colWidths: [13, 35, 30, 13, 13],
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [
+                    res[i].item_id,
+                    res[i].product_name,
+                    res[i].department_name,
+                    "$ " + res[i].price,
+                    res[i].stock_quantity
+                ]
+            );
+        };
+
+        console.log(table.toString());
+
+        buy();
+
+
+    });
+}
+
+function buy() {
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "item",
+                message: "What is the ID of the product that you would like to buy?",
+
+            },
+            {
+                type: "input",
+                name: "amount",
+                message: "How many units of the product would you like to buy?",
+
+            }
+        ])
+        .then(function (answer) {
+            var itemId = answer.item;
+            var amount = answer.amount;
+
+            connection.query("SELECT * FROM products WHERE item_id=?", itemId, function (err, res) {
+                for (var i = 0; i < res.length; i++) {
+
+                    if (amount > res[i].stock_quantity) {
+
+                        console.log("===================================================");
+                        console.log("Insufficient quantity!");
+                        console.log("===================================================");
+                        showProducts();
+
+                    } else {
+                        //list item information for user for confirm prompt
+                        console.log("==============================");
+                        console.log("We can fulfull your order.");
+                        console.log("==============================");
+                        console.log("You've selected:");
+                        console.log("----------------");
+                        console.log("Item: " + res[i].product_name);
+                        console.log("Department: " + res[i].department_name);
+                        console.log("Price: $" + res[i].price);
+                        console.log("Quantity: " + amount);
+                        console.log("----------------");
+                        console.log("Total: $" + res[i].price * amount);
+                        console.log("===============================");
+
+                        var newStock = (res[i].stock_quantity - amount);
+
+                        //console.log(newStock);
+                        confirmPrompt(newStock, itemId);
+                    }
+                }
+            });
+
+
+
+
+
+
+
+        });
+
+
+
+
+}
+
+function confirmPrompt(newStock, itemId) {
+
+    inquirer.prompt([{
+
+        type: "confirm",
+        name: "confirmPurchase",
+        message: "Are you sure you would like to purchase this item and quantity?",
+        default: true
+
+    }]).then(function (userConfirm) {
+        if (userConfirm.confirmPurchase === true) {
+
+            //if user confirms purchase, update mysql database with new stock quantity by subtracting user quantity purchased.
+
+            connection.query("UPDATE products SET ? WHERE ?", [{
+                stock_quantity: newStock
+            }, {
+                item_id: itemId
+            }], function (err, res) {});
+
+            console.log("=================================");
+            console.log("Transaction completed. Thank you.");
+            console.log("=================================");
+            showProducts();;
+        } else {
+            console.log("=================================");
+            console.log("No worries. Maybe next time!");
+            console.log("=================================");
+            showProducts();
+        }
+    });
+
+}
